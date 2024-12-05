5e5adbc804e006c96f5d47257f653a31e0b86def
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-12-05 14:45:15 -0300

    feat: create `POST` endpoint to `Post` Model

--- codeleap_backend/posts/serializers.py
+++ codeleap_backend/posts/serializers.py
@@ -6,3 +6,20 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["id", "username", "created_datetime", "title", "content"]
+
+    def validate_username(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError(
+                "Username must be at least 3 characters long."
+            )
+        return value
+
+    def validate_title(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Title cannot be empty.")
+        return value
+
+    def validate_content(self, value):
+        if not value.strip():
+            raise serializers.ValidationError("Title cannot be empty.")
+        return value
--- codeleap_backend/posts/views.py
+++ codeleap_backend/posts/views.py
@@ -1,9 +1,17 @@
-from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Post
 from .serializers import PostSerializer
 
 
-class PostViewSet(ReadOnlyModelViewSet):
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all().order_by("-created_datetime")
     serializer_class = PostSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
