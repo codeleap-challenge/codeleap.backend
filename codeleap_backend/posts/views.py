7efebe0348e9d3d8d80461c8e0d8dc5426b7ff1d
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-12-05 14:30:17 -0300

    feat: create `GET` Endpoint to `post` Model

--- codeleap_backend/posts/views.py
+++ codeleap_backend/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+
+class PostViewSet(ReadOnlyModelViewSet):
+    queryset = Post.objects.all().order_by("-created_datetime")
+    serializer_class = PostSerializer
