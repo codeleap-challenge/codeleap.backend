01183aa6eec791c700a172110d4dc4b18d96a71f
lucasxaviervieira <lucasdavid.xavier@gmail.com>
2024-12-05 17:09:20 -0300

    chore: add wsgi to `Render`

--- codeleap_backend/codeleap_backend/settings.py
+++ codeleap_backend/codeleap_backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -127,3 +128,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
